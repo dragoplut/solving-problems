5a42ecc8a06bc5d6fcf943620fed45585768ea4a
dragoplut <oleksandr_p@i.ua>
2016-01-09 17:47:34 +0200

    add calc with Brackets & split stringToArr from calculateNewArray

--- js/task2.js
+++ js/task2.js
@@ -3,34 +3,14 @@
  */
 function inputValue(){
     var someString = document.getElementById('calcInput').value;
-    var result = calculateString(someString);
+    var result = processingBrackets(someString);
     console.log(result);
     alert(result);
 }
 
-function calculateString(someSring){ //calculates input string, allowed:  0123456789.+-*/  returns: "result" ;
-    var inputString = someSring;
-    var k = 0;
+function calculateNewArray(someArray){ //calculates input array, allowed:  0123456789.+-*/  returns: "result" ;
     var numberForCount = '';
-    var stringArr = [];
-    for (var i = 0; i < inputString.length; i++){
-        if (inputString[i] === '+' || inputString[i] === '-' || inputString[i] === '*' || inputString[i] === '/') {
-            k += 1;
-            stringArr[k] = inputString[i];
-            k += 1;
-        }
-        else if (inputString[i] === undefined){
-            alert('Введено некоректні дані!');
-            return;
-        }
-        else {
-            if (stringArr[k] === undefined){
-                stringArr[k] = '';
-            }
-            stringArr[k] += inputString[i];
-        }
-    }
-
+    var stringArr = someArray;
     while (stringArr.length > 1){
         if (stringArr.indexOf('*') > 0 || stringArr.indexOf('/') > 0){
             if (stringArr.indexOf('*') < stringArr.indexOf('/') && stringArr.indexOf('*') != -1 || stringArr.indexOf('/') == -1 || stringArr.indexOf('/') == false){
@@ -65,4 +45,69 @@ function calculateString(someSring){ //calculates input string, allowed:  012345
         }
     }
     return(stringArr[0]);
+}
+
+function stringToArr(someString){   // convert string to math split array, allowed: 0123456789()+-*/  , returns: array
+    var inputString = someString;
+    var k = 0;
+    var stringArr = [];
+    for (var i = 0; i < inputString.length; i++){
+        if (inputString[i] === '+' || inputString[i] === '-' || inputString[i] === '*' || inputString[i] === '/') {
+            if (inputString[i-1] === ')'){
+                stringArr[k] = inputString[i];
+                k += 1;
+            } else if (inputString[i+1] === '('){
+                k += 1;
+                stringArr[k] = inputString[i];
+            } else {
+            k += 1;
+            stringArr[k] = inputString[i];
+            k += 1;
+            }
+        }
+        else if (inputString[i] === '(' || inputString[i] === ')'){
+            if (inputString[i] === '(' && inputString[i-1] === undefined || inputString[i] === '(' && inputString[i-1] === '(' || inputString[i] === ')' && inputString[i-1] === ')'){
+                stringArr[k] = inputString[i];
+                k += 1;
+            } else if (inputString[i-1] != undefined){
+                k += 1;
+                stringArr[k] = inputString[i];
+                k += 1;
+            }
+        }
+        else {
+            if (stringArr[k] === undefined){
+                stringArr[k] = '';
+            }
+            stringArr[k] += inputString[i];
+        }
+    }
+    return(stringArr);
+}
+
+function processingBrackets (someString){   // calculates input string "with brackets", returns: result. Warning!!! Needed functions: stringToArr(someString) && calculateNewArray(someArray)
+    var inputArr = stringToArr(someString);
+    var stringArr = [];
+    var firstCloseBracket = inputArr.indexOf(')');
+    var result = '';
+    while (inputArr.length > 1){
+        if (inputArr.indexOf(')') && inputArr.indexOf(')') != -1){
+            for (var i = (firstCloseBracket-1); i > -1; i--){
+                if (inputArr[i] === '('){
+                    stringArr = [];
+                    stringArr = inputArr.slice(i + 1, firstCloseBracket);
+                    inputArr.splice(i, firstCloseBracket-i+1, calculateNewArray(stringArr));
+                    firstCloseBracket = inputArr.indexOf(')');
+                    if (inputArr.indexOf(')') == -1){
+                        i = -1;
+                    }
+                }
+            }
+        } else if (inputArr.length > 1){
+            result = calculateNewArray(inputArr);
+        } else {
+            result = inputArr;
+        }
+    }
+    return (result);
 }
